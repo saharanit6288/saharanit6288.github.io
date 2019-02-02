d69ad0dfb42cad635c580b8f90d9b569c7307979
Ronit <saharanit321@gmail.com>
2019-02-02 11:08:46 +0530

    boards app deployed

---
+++ boards-app/precache-manifest.c08d4b2f22b9790d1bf90faf4ae14483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/boards-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2806458531b37e608b51",
+    "url": "/boards-app/static/js/runtime~main.28064585.js"
+  },
+  {
+    "revision": "81a60c8b103365dc5408",
+    "url": "/boards-app/static/js/main.81a60c8b.chunk.js"
+  },
+  {
+    "revision": "147793461b6804012594",
+    "url": "/boards-app/static/js/1.14779346.chunk.js"
+  },
+  {
+    "revision": "81a60c8b103365dc5408",
+    "url": "/boards-app/static/css/main.7ea88256.chunk.css"
+  },
+  {
+    "revision": "147793461b6804012594",
+    "url": "/boards-app/static/css/1.262a6a0a.chunk.css"
+  },
+  {
+    "revision": "991549b3e0c9620612f8457b04ac8175",
+    "url": "/boards-app/index.html"
+  }
+];
