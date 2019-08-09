7cc918c1172e691e473df5c10cc8f5dac11151a9
Ronit <saharanit321@gmail.com>
2019-08-09 08:52:20 +0530

    react chat app first commit

---
+++ react-chat-app/precache-manifest.0ae5c11b9361ca65ab97d4cca384af24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f999a54c2c22cda37723b8c2bb90c8",
+    "url": "/react-chat-app/index.html"
+  },
+  {
+    "revision": "402b7ac03322acd1d243",
+    "url": "/react-chat-app/static/css/main.998b3b96.chunk.css"
+  },
+  {
+    "revision": "231d1646891035d8d003",
+    "url": "/react-chat-app/static/js/2.2e273edd.chunk.js"
+  },
+  {
+    "revision": "402b7ac03322acd1d243",
+    "url": "/react-chat-app/static/js/main.ea79520b.chunk.js"
+  },
+  {
+    "revision": "911d676f5e0112a7c96d",
+    "url": "/react-chat-app/static/js/runtime~main.163dfad8.js"
+  }
+]);
