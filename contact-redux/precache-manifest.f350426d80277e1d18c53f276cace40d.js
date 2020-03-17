1333d8f471b6cfec10129b996caea7ab0e2ee718
Ranit Saha <saharanit321@gmail.com>
2020-03-17 15:31:30 +0530

    react redux contact form webapp deploy

---
+++ contact-redux/precache-manifest.f350426d80277e1d18c53f276cace40d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ae51830632db7defd1dca09906f410",
+    "url": "/contact-redux/index.html"
+  },
+  {
+    "revision": "556f28c8cc2fc251ab35",
+    "url": "/contact-redux/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "34152f483f721537f49e",
+    "url": "/contact-redux/static/js/2.0d8544e4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/contact-redux/static/js/2.0d8544e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "556f28c8cc2fc251ab35",
+    "url": "/contact-redux/static/js/main.45068ce7.chunk.js"
+  },
+  {
+    "revision": "154980fc4429ee4a799a",
+    "url": "/contact-redux/static/js/runtime-main.c6bb11be.js"
+  }
+]);
